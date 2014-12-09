54df8cc94574b626b9509e85b1c6394bd570575a
townboy <564690377@qq.com>
2014-12-09 16:11:10 +0800

    nothing

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from bottle import run, route
+from bottle import run,\
+     route,\
+     Bottle,\
+     debug,\
+     jinja2_template,\
+     static_file
+import sys
+import codecs
+import markdown
+import settings
 
-@route('/')
+
+application = Bottle()
+debug(True)
+
+@application.route('/')
 def index():
-    return 'hello kitty'
+    return jinja2_template('templates/home.html', domain = settings.domain)
 
-run(host = 'localhost', port = 8080)
+@application.route('/static/<filename:path>')
+def static(filename):
+    return static_file(filename, root = './static')
+
+if __name__ == '__main__':
+    run(application, host = settings.host, port = settings.port)
